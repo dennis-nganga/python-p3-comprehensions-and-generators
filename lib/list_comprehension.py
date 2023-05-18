9b4e908fce9da9e5a4eb4da2c59abbe0c74210e2
dennis-nganga <denniskimani416@gmail.com>
2023-05-18 21:28:31 +0300

    completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = []
+    for num in num_list:
+        if num % 2 == 0:
+            even_list.append(num)
+    return even_list
 
+   
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) == 0:
+        return []
+    
+    exclamation_list = []
+    for sentence in sentence_list:
+        if sentence[-1] in ['.', '!', '?']:
+            exclamation_sentence = sentence[:-1] + "!"
+        else:
+            exclamation_sentence = sentence + "!"
+        exclamation_list.append(exclamation_sentence)
+
+    return exclamation_list
